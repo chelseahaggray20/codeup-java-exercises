1234659c1ac0900f7f1adff8eecaffe7828a36ac
Chelseahaggray20 <chelsea.haggray20@gmail.com>
2020-01-27 14:15:00 -0600

    first java exercise

---
+++ src/HelloWorld.java
@@ -0,0 +1,35 @@
+public class HelloWorld {
+//    public static void main(String[] args) {
+//       System.out.print("Hello, World");
+//
+//       byte num1 = 127;
+//       short num2 = num1;
+//       int num3 = num2;
+//       long num4 = num3;
+//        System.out.println(num4);
+//
+//       float num5 = 3.3333f;
+//        double num6 = num5;
+//
+//        boolean yesNo = true;
+//        yesNo = false;
+//
+//        char initial;
+//        initial = 'D';
+
+
+//        String.name = "chelsea";
+
+
+    public static void main(String[] args) {
+        int myFavoriteNumber = 21;
+        System.out.println(myFavoriteNumber);
+    }
+
+
+
+
+
+}
+
+

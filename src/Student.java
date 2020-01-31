2b6eb8c841a90aac55101383e0769bfadfeaed75
Chelseahaggray20 <chelsea.haggray20@gmail.com>
2020-01-31 10:42:42 -0600

    new Person exercise

---
+++ src/Student.java
@@ -0,0 +1,41 @@
+import java.util.Date;
+
+public class Student {
+    String name;
+    String cohort;
+    Date startDate;
+    String program;
+    String location;
+    boolean drinksCoffee;
+
+
+    public static void main(String[] args) {
+        Student daniel = new Student();
+        daniel.name = "Daniel";
+        daniel.cohort = "Europa";
+        daniel.startDate = new Date("06/11/2018");
+        daniel.program = "Web Development";
+        daniel.location = "San Antonio";
+        daniel.drinksCoffee = true;
+
+        Student zach = new Student();
+        zach.name = "Zach";
+        zach.cohort = "Bayes";
+        zach.startDate = new Date("February 11, 2017");
+        zach.program = "Data Science";
+        zach.location = "San Antonio";
+        zach.drinksCoffee = false;
+
+        System.out.println(daniel.name);
+        if (zach.drinksCoffee)
+            System.out.println(zach.name + "drinks coffee.");
+    }
+}
+
+//    public String report() {
+//        String output = "";
+//        output += "My name is " + this.name +". ";
+//        output += "I started learning " + this.program + " with " + this.cohort + " on " + this.startDate + " . ";
+////        if (this.drinksCoffee);
+//    }
+//}

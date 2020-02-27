f9dabbe589c2f29260da00bb34779feaae83d999
chelseahaggray20 <57374227+chelseahaggray20@users.noreply.github.com>
2020-02-27 16:57:27 -0600

    updated shapes test

--- src/shapes/ShapesTest.java
+++ src/shapes/ShapesTest.java
@@ -1,35 +1,28 @@
-//package shapes;
-//
-//public class ShapesTest {
-//    public static void main(String[] args) {
-//        Measurable myShape;
-//
-//
-////        myShape = new Square(int 6,3);
-////        System.out.println(myShape.getArea());
-////        System.out.println(myShape.getPerimeter());
-////        System.out.println(myShape instanceof Square);
-////        System.out.println(myShape instanceof Measurable);
-////
-////        myShape = new Rectangle(6.3, 3.5);
-////        System.out.println(myShape.getPerimeter());
-////        System.out.println(myShape.getArea());
-////        System.out.println(myShape instanceof  Square);
-////
-//////        @Override
-////            public double getPerimeter() {
-////                return 0;
-////            }
-////
-////            @Override
-////            public double getArea() {
-////                return 0;
-////            }
-//        };
-//
-//
-//////        Measurable myShape2 = new Square(double, double);
-////        System.out.println(myShape2.getPerimeter());
-////        System.out.println(myShape2.getArea());
-//    }
-//}
+package shapes;
+
+public class ShapesTest {
+    public static void main(String[] args){
+        Rectangle box1 = new Rectangle(5,4);
+        System.out.println(box1.getPerimeter());
+        System.out.println(box1.getArea());
+
+//        Rectangle box2 = new Square(5);
+//        System.out.println(box2.getPerimeter());
+//        System.out.println(box2.getArea());
+
+        Measurable myShape;
+
+        myShape = new Square(6.3);
+        System.out.println(myShape.getArea());
+        System.out.println(myShape.getPerimeter());
+//        myShape.getLength();
+        System.out.println(myShape instanceof Square);
+        System.out.println(myShape instanceof Measurable);
+
+        myShape = new Rectangle(3.3, 5.5);
+        System.out.println(myShape.getArea());
+        System.out.println(myShape.getPerimeter());
+//        myShape.getLength();
+        System.out.println(myShape instanceof Square);
+    }
+}

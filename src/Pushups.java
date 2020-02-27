2371af2fdced46182fd45c106397911e2817f6fe
Chelseahaggray20 <chelsea.haggray20@gmail.com>
2020-02-27 16:34:55 -0600

    morning pushups

--- src/Pushups.java
+++ src/Pushups.java
@@ -1,7 +1,19 @@
-public class Pushups {
-
-}
-
-
-
-//write a java program that reads in a series of 3-5 lines and then writes them out to the console
+//
+//public class Pushups {
+//
+//}
+//
+//
+//
+////write a java program that reads in a series of 3-5 lines and then writes them out to the console
+//
+//
+////This is a method that takes in a list of Integers and an int. It parses the list to find whcih pairs of gets in the list when added together equals the int parameter. Return the result as an ArrayList of ArrayLists of Integer.
+//
+//public static ArrayList<ArrayList<Integer>> findAddends(){
+//    ArrayList<ArrayList<Integer>> sums = new ArrayList<>();
+//
+//
+//    return sums;
+//
+//}
---
+++ src/testing.java
@@ -0,0 +1,21 @@
+import jdk.jfr.StackTrace;
+
+import java.util.ArrayList;
+
+public class testing {
+    @Before
+
+    public void setup(){
+        ArrayList<Integer> intList = new ArrayList<>();
+        intList.add(1);
+        intList.add(2);
+        intList.add(3);
+        intList.add(4);
+        intList.add(5);
+
+    }
+
+    @Test
+    public void
+
+}
